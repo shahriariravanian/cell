bdd886bf5fabc0bd6ba59ed4f61d60e8e307aa90
Shahriar Iravanian <siravan@svtsim.com>
2024-12-18 20:35:22 -0500

    Lorenz eq working!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::{Write, BufWriter};
+
 mod codegen;
 mod register;
 mod system;
@@ -6,6 +9,23 @@ mod code;
 use crate::codegen::test_codegen;
 use crate::system::*;
 
+fn solve(prog: &Program) {
+    let mut fun = Function::new(&prog);
+    let mut u = fun.initial_states();
+    let mut du = u.clone();
+    
+    let mut fd = File::create("test.dat").expect("cannot open the file");
+    let mut buf = BufWriter::new(fd);
+    
+    for i in 0..5000 {    
+        writeln!(&mut buf, "{}\t{}\t{}", u[0], u[1], u[2]);
+        fun.call(&mut du, &u, 0.0);
+        for j in 0..3 {
+            u[j] += 0.01 * du[j];
+        }
+    }
+}
+
 fn main() {
     // test_codegen();
     let sys = load_system("julia/test.json").unwrap();
@@ -13,5 +33,12 @@ fn main() {
     let mut prog = Program::new(&sys);   
     sys.lower(&mut prog);
     println!("{:#?}", prog);    
+    println!("ns = {}", prog.frame.count_states());
+    println!("np = {}", prog.frame.count_params());
+    println!("no = {}", prog.frame.count_obs());
+    println!("s = {}", prog.frame.first_state().unwrap());
+    println!("p = {}", prog.frame.first_param().unwrap());
+ 
+    solve(&prog);
 }
 
--- src/system.rs
+++ src/system.rs
@@ -7,15 +7,22 @@ use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
+
+use std::rc::Rc;
+use std::cell::RefCell;
 
 use crate::register::*;
 use crate::code::*;
 
 
+pub trait Lower {
+    fn lower(&self, prog: &mut Program) -> Reg;
+}
+
 #[derive(Debug)]
 pub struct Program {
-    pub prog:   Vec<Instruction>,
+    pub code:   Rc<RefCell<Vec<Instruction>>>,
     pub frame:  Frame
 }
 
@@ -23,7 +30,7 @@ impl Program {
     pub fn new(sys: &System) -> Program {
         let mut frame = Frame::new();
 
-        frame.alloc(RegType::State(sys.iv.name.clone()), None);
+        frame.alloc(RegType::Var(sys.iv.name.clone()), None);
         
         for v in &sys.states {
             frame.alloc(RegType::State(v.name.clone()), Some(v.val));
@@ -38,13 +45,13 @@ impl Program {
         }                
     
         Program {
-            prog:   Vec::new(),
+            code:   Rc::new(RefCell::new(vec![])),
             frame
         }
     }
     
     pub fn push(&mut self, s: Instruction) {
-        self.prog.push(s)
+        self.code.borrow_mut().push(s)
     }
     
     pub fn alloc_temp(&mut self) -> Reg {
@@ -60,14 +67,10 @@ impl Program {
     }
     
     pub fn reg(&self, name: &str) -> Reg {        
-        if let Some(r) = self.frame.find(&RegType::State(name.to_string())) {
-            return r;
-        }
-        if let Some(r) = self.frame.find(&RegType::Param(name.to_string())) {
-            return r;
-        }
-        if let Some(r) = self.frame.find(&RegType::Obs(name.to_string())) {
-            return r;
+        for reg_type in [RegType::State, RegType::Param, RegType::Obs, RegType::Var] {
+            if let Some(r) = self.frame.find(&reg_type(name.to_string())) {
+                return r;
+            }
         }
         panic!("cannot find reg by name");
     }
@@ -80,9 +83,67 @@ impl Program {
     }
 }
 
-pub trait Lower {
-    fn lower(&self, prog: &mut Program) -> Reg;
+#[derive(Debug)]
+pub struct Function {
+    pub code:           Rc<RefCell<Vec<Instruction>>>,    
+    pub mem:            Vec<f64>,    
+    pub u0:             Vec<f64>,
+    pub first_state:    usize,
+    pub count_states:   usize,
+    pub first_param:    usize,
+    pub count_params:   usize
 }
+
+impl Function {
+    pub fn new(prog: &Program) -> Function {
+        let code = prog.code.clone();
+        let mem = prog.frame.mem();
+        
+        let first_state = prog.frame.first_state().unwrap();
+        let count_states = prog.frame.count_states();
+        let first_param = prog.frame.first_param().unwrap();
+        let count_params = prog.frame.count_params();
+        
+        let u0 = &mem[first_state..first_state+count_states].to_vec();
+
+        Function {
+            code,
+            mem,
+            u0: u0.clone(),
+            first_state,
+            count_states,
+            first_param,
+            count_params
+        }
+    }
+    
+    pub fn initial_states(&self) -> Vec<f64> {
+        self.u0.clone()
+    }
+    
+    pub fn call(&mut self, du: &mut Vec<f64>, u: &Vec<f64>, t: f64) {
+        self.mem[2] = t;    // TODO: hardcoded iv address        
+        
+        let mut p = &mut self.mem[self.first_state..self.first_state+self.count_states];
+        p.copy_from_slice(&u[..]);
+        
+        self.run();        
+        
+        let dp = &self.mem[self.first_state+self.count_states..self.first_state+2*self.count_states];
+        &mut du[..].copy_from_slice(dp);
+    }
+    
+    pub fn run(&mut self) {
+        for c in self.code.borrow().iter() {
+            match c {
+                Instruction::Op { f, x, y, dst, .. } => { self.mem[dst.0] = f(self.mem[x.0], self.mem[y.0]); },
+                Instruction::Num { val, dst } => { self.mem[dst.0] = *val; },
+                Instruction::Var { name, reg } => {},
+            }
+        }
+    }
+}
+
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct Variable {
@@ -230,9 +291,15 @@ impl Lower for Expr {
     fn lower(&self, prog: &mut Program) -> Reg {
         match self {
             Expr::Const { val } => {
-                let dst = prog.alloc_temp();
-                prog.push(Instruction::Num { val: *val, dst });
-                dst
+                if *val == 0.0 {
+                    Reg(0)
+                } else if *val == -1.0 {
+                    Reg(1)
+                } else{
+                    let dst = prog.alloc_temp();
+                    prog.push(Instruction::Num { val: *val, dst });
+                    dst
+                }
             },
             Expr::Var { name } => {
                 let dst = prog.reg(name);
@@ -282,13 +349,6 @@ pub struct System {
     pub algs: Vec<Equation>,
     pub odes: Vec<Equation>,
     pub obs: Vec<Equation>,
-}
-
-impl System {
-    pub fn reg_base(&self) -> Reg {
-        //Reg(2 + 2 * self.states.len() + self.params.len() + self.obs.len())
-        Reg(2 + 2 * self.states.len() + self.params.len())
-    }
 }
 
 impl Lower for System {
