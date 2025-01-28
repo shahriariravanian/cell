a520769545d80e3792aa217086174602c249991a
Shahriar Iravanian <siravan@svtsim.com>
2025-01-27 20:55:11 -0500

    machine and analyzer separated

---
+++ src/analyzer.rs
@@ -0,0 +1,119 @@
+use std::collections::HashSet;
+
+use super::code::Instruction;
+use super::model::Program;
+use super::register::Word;
+
+pub enum Event {
+    Producer(Word),
+    Consumer(Word),
+    Caller(String),
+}
+
+pub struct Analyzer {
+    pub events: Vec<Event>
+}
+
+impl Analyzer {
+    pub fn new(prog: &Program) -> Self {
+        let mut events: Vec<Event> = Vec::new();
+
+        for c in prog.code.iter() {
+            match c {
+                Instruction::Unary { op, x, dst, .. } => {
+                    events.push(Event::Consumer(*x));
+                    events.push(Event::Caller(op.clone()));
+                    events.push(Event::Producer(*dst));
+                }
+                Instruction::Binary { op, x, y, dst, .. } => {
+                    events.push(Event::Consumer(*x));
+                    events.push(Event::Consumer(*y));
+                    events.push(Event::Caller(op.clone()));
+                    events.push(Event::Producer(*dst));
+                }
+                Instruction::IfElse { x1, x2, cond, dst } => {
+                    events.push(Event::Consumer(*x1));
+                    events.push(Event::Consumer(*x2));
+                    events.push(Event::Consumer(*cond));
+                    events.push(Event::Caller("select".to_string()));
+                    events.push(Event::Producer(*dst));
+                }
+                _ => {}
+            }
+        };
+        
+        Self { events }
+    }
+
+    /*
+        A saveable register is produced but is not consumed immediately
+        In other words, it cannot be coalesced over consecuative instructions
+    */
+    pub fn find_saveables(&self) -> HashSet<Word> {
+        let mut candidates: Vec<Word> = Vec::new();
+        let mut saveables: HashSet<Word> = HashSet::new();
+
+        for l in self.events.iter() {
+            match l {
+                Event::Producer(p) => {
+                    candidates.push(*p);
+                }
+                Event::Consumer(c) => {
+                    let r = candidates.pop();
+
+                    if candidates.contains(c) {
+                        saveables.insert(*c);
+                    };
+
+                    if r.is_some() {
+                        candidates.push(r.unwrap());
+                    };
+                }
+                Event::Caller(_) => {}
+            }
+        }
+
+        saveables
+    }
+
+    /*
+        A bufferable register is a saveable register that its lifetime
+        does not cross an external call boundary, which can invalidate
+        the buffer
+    */
+    pub fn find_bufferable(&self) -> HashSet<Word> {
+        let caller = [
+            "rem", "power", "sin", "cos", "tan", "csc", "sec", "cot", "arcsin", "arccos", "arctan",
+            "exp", "ln", "log", "root",
+        ];
+
+        let mut candidates: Vec<Word> = Vec::new();
+        let mut bufferable: HashSet<Word> = HashSet::new();
+
+        for l in self.events.iter() {
+            match l {
+                Event::Producer(p) => {
+                    candidates.push(*p);
+                }
+                Event::Consumer(c) => {
+                    let r = candidates.pop();
+
+                    if candidates.contains(c) {
+                        bufferable.insert(*c);
+                    };
+
+                    if r.is_some() {
+                        candidates.push(r.unwrap());
+                    };
+                }
+                Event::Caller(op) => {
+                    if caller.contains(&op.as_str()) {
+                        candidates.clear();
+                    }
+                }
+            }
+        }
+
+        bufferable
+    }
+}
---
+++ src/machine.rs
@@ -0,0 +1,65 @@
+use memmap2::{Mmap, MmapOptions};
+use rand::distributions::{Alphanumeric, DistString};
+use std::fs;
+use std::io::Write;
+
+use super::utils::*;
+use super::code::BinaryFunc;
+
+#[derive(Debug)]
+pub struct MachineCode {
+    p: *const u8,
+    mmap: Mmap, // we need to store mmap and fs here, so that they are not dropped
+    name: String,
+    fs: fs::File,
+    vt: Vec<BinaryFunc>,
+    _mem: Vec<f64>,
+}
+
+impl MachineCode {
+    pub fn new(machine_code: &Vec<u8>, vt: Vec<BinaryFunc>, _mem: Vec<f64>) -> MachineCode {
+        let name = Alphanumeric.sample_string(&mut rand::thread_rng(), 16) + ".bin";
+        MachineCode::write_buf(machine_code, &name);
+        let fs = fs::File::open(&name).unwrap();
+        let mmap = unsafe { MmapOptions::new().map_exec(&fs).unwrap() };
+        let p = mmap.as_ptr() as *const u8;
+
+        MachineCode {
+            p,
+            mmap,
+            name,
+            fs,
+            vt,
+            _mem,
+        }
+    }
+
+    fn write_buf(machine_code: &Vec<u8>, name: &str) {
+        let mut fs = fs::File::create(name).unwrap();
+        fs.write(machine_code).unwrap();
+    }
+}
+
+impl Compiled for MachineCode {
+    fn run(&mut self) {
+        let f: fn(&[f64], &[BinaryFunc]) = unsafe { std::mem::transmute(self.p) };
+        f(&mut self._mem, &self.vt);
+    }
+
+    #[inline]
+    fn mem(&self) -> &[f64] {
+        &self._mem[..]
+    }
+
+    #[inline]
+    fn mem_mut(&mut self) -> &mut [f64] {
+        &mut self._mem[..]
+    }
+}
+
+impl Drop for MachineCode {
+    fn drop(&mut self) {
+        let _ = fs::remove_file(&self.name);
+    }
+}
+
